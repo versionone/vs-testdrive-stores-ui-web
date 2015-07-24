3c8b26ec3a25d34370bef0cc2fd42ae1d307c306
Demo User <support@clearcodelabs.com>
2015-07-24 14:05:40 -0700

    moved artifact storage out of package script

--- tools/package.sh
+++ tools/package.sh
@@ -3,14 +3,11 @@ set -e
 
 # create the artifact
 PRODUCT=stores-ui
-FILENAME=${PRODUCT}-${BUILD_NUMBER}
+FILENAME=${PRODUCT}-${BUILD_NUMBER}.tz
 mkdir -p artifact/${PRODUCT}
 cp -R browserconfig.xml cart.html category.html contact.html css img index.html js new-account.html order-details.html order-history.html personal-details.html product.html search-result.html signin.html artifact/${PRODUCT}
 
 cd artifact
 
-tar -czf ${FILENAME}.tar.gz ${PRODUCT}
-
-set +x
-echo "[CCLARTIFACT:{\"name\": \"${PRODUCT}\", \"location\": \"https://s3.amazonaws.com/builds.clearcodelabs.com/${PRODUCT}/${FILENAME}.tar.gz\"}]"
+tar -czf ${FILENAME} ${PRODUCT}
 echo "done!"
