09ee94a780f8c85bcf243e7156d0a04a90945620
Administrator <admin@example.com>
2016-04-26 14:13:33 -0700

    changed packaging script

--- tools/package.sh
+++ tools/package.sh
@@ -6,5 +6,4 @@ PRODUCT=stores-ui-web
 FILENAME=${PRODUCT}.tz
 mkdir -p artifact
 tar -czf artifact/${FILENAME} browserconfig.xml cart.html category.html contact.html css img index.html js new-account.html order-details.html order-history.html personal-details.html product.html search-result.html signin.html
-
 echo "done!"
