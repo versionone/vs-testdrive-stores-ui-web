f832e85f34d338b20b94750c85d258b88ff75f7e
Administrator <admin@example.com>
2016-04-26 14:16:26 -0700

    change config

--- tools/package.sh
+++ tools/package.sh
@@ -2,7 +2,7 @@
 set -e
 
 # create the artifact
-PRODUCT=stores-ui-web
+PRODUCT=stores-ui
 FILENAME=${PRODUCT}.tz
 mkdir -p artifact
 tar -czf artifact/${FILENAME} browserconfig.xml cart.html category.html contact.html css img index.html js new-account.html order-details.html order-history.html personal-details.html product.html search-result.html signin.html
